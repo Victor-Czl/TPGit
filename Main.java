c96617e3fb3a2e89b44519f889e8704c8ec3278f
Victor-Czl <118814687+Victor-Czl@users.noreply.github.com>
2022-12-05 09:15:10 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
   public static main (String[]args) {
   
-    System.out.println("Good morning world, how are you ?");
+    System.out.println("Good morning world ! how are you ?");
     
   }
